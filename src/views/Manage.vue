b9854793c1e089a906b5707ede0f1fa40991a279
rubenberna <rubenmbernardes@gmail.com>
2018-09-07 00:17:04 +0200

    feat(): added floating button to create new therapy in the management page

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="manage">
-    Management page
+    <h3>Management page</h3>
+    <div class="fixed-action-btn">
+      <router-link to='/new' class="btn-floating btn-large green">
+        <i class="fa fa-plus"></i>
+      </router-link>
+    </div>
   </div>
 </template>
 
