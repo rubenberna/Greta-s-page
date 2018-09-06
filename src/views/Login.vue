abe9f753e60c7f59f1cb7bcca8505029ac3990eb
rubenberna <rubenmbernardes@gmail.com>
2018-09-06 13:20:58 +0200

    feat(): login component

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,22 @@
 <template>
-  <div class="Login">
-    Login please
+  <div class="login container">
+    <div class="login-title">
+      <h2>Sign-in</h2>
+    </div>
+    <sui-form>
+  <sui-form-field>
+    <label>Email address</label>
+    <input placeholder="Email address" >
+  </sui-form-field>
+  <sui-form-field>
+    <label>Password</label>
+    <input placeholder="Password" >
+  </sui-form-field>
+  <sui-form-field>
+    <p>No account yet? You can create one</p>
+  </sui-form-field>
+  <sui-button type="submit">Enter</sui-button>
+</sui-form>
   </div>
 </template>
 
@@ -11,4 +27,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .login {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100vh;
+    width: 350px;
+    .login-title {
+      text-align: center;
+      margin-bottom: 20px;
+      font-weight: bold;
+    }
+  }
 </style>
