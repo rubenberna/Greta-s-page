6c95fb64360cf50f7fa850f4c5ed1d2327fe7595
rubenberna <rubenmbernardes@gmail.com>
2018-09-22 21:26:08 +0200

    feat(): booking modal with therapy prop and css image in allTherapies -- not concluded yet

--- src/components/AllTherapies.vue
+++ src/components/AllTherapies.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="all-therapies container">
-    <div v-for="therapy in therapies"
-         :key="therapy.id">
     <div class="all-therapies-label">
       <h5>Treatments</h5>
     </div>
+    <div v-for="therapy in therapies"
+         :key="therapy.id">
     <div class="all-therapies-card">
       <div class="all-therapies-card-image">
         <sui-image :src='therapy.image' size="large" />
@@ -12,27 +12,31 @@
       <div class="all-therapies-card-description">
         <h1>{{therapy.teaser}}</h1>
         <div class="all-therapies-card-description-availability">
-          <span>Make appointment</span>
+          <Booking :therapy='therapy'></Booking>
           <span>{{therapy.availability}}</span>
         </div>
       </div>
     </div>
-    <p class="all-therapies-card-text">
+    <!-- <p class="all-therapies-card-text">
       {{therapy.indications}}.
     </p>
     <span style="font-weight:bold">Read More <font-awesome-icon icon="coffee" /> </span>
     <br>
-    <br>
+    <br> -->
     </div>
   </div>
 </template>
 
 <script>
   import { mapGetters } from 'vuex'
+  import Booking from '@/components/Booking'
 
   export default {
     name: 'allTherapies',
-    computed: mapGetters(['therapies'])
+    computed: mapGetters(['therapies']),
+    components: {
+      Booking
+    }
   }
 </script>
 
@@ -55,12 +59,35 @@
   .all-therapies-card {
     display: flex;
     max-width: 750px;
-    height: 300px;
+    height: 350px;
     justify-content: space-between;
+    margin-bottom: 78px;
 
     .all-therapies-card-image {
-     width: 45%;
-     max-height: inherit;
+     width: 50%;
+     height: inherit;
+     margin-right: 1px;
+     position: relative;
+     overflow: hidden;
+     display: inline-block;
+     // background-position: center center;
+     // background-repeat: no-repeat;
+     // overflow: hidden;
+     // object-fit: cover;
+     // object-position: center;
+     // background-size: cover;
+     img {
+       position: absolute;
+       left: 50%;
+       top: 50%;
+       width: 90%;
+       height: auto;
+       -webkit-transform: translate(-50%,-50%);
+           -ms-transform: translate(-50%,-50%);
+               transform: translate(-50%,-50%);
+       // width: 100%;
+       // height: auto;
+     }
     }
 
     .all-therapies-card-description {
@@ -80,18 +107,15 @@
 
       .all-therapies-card-description-availability {
         display: flex;
-        justify-content: center;
+        justify-content: space-between;
         align-items: baseline;
         span {
           padding: 9px 15px;
           font-size: 12px;
           letter-spacing: .10rem;
           text-transform: uppercase;
-          border-right: 1px solid #e5e5e5;
-          border-bottom: 1px solid #e5e5e5;
-          &:last-child {
-            margin-left: 9px;
-          }
+          border-right: 1px solid $light-grey;
+          border-bottom: 1px solid $light-grey;
         }
       }
     }
---
+++ src/components/Booking.vue
@@ -0,0 +1,61 @@
+<template lang="html">
+  <div class="booking container">
+    <sui-button  class="booking-button"
+                 @click.native="toggle">Make appointment</sui-button>
+    <sui-modal v-model="open">
+      <sui-modal-header>Select a Photo</sui-modal-header>
+      <sui-modal-content image>
+        <sui-image wrapped size="medium" :src='therapy.image' />
+        <sui-modal-description>
+          <sui-header>Default Profile Image</sui-header>
+          <p>We've found the following gravatar image associated with your e-mail address.</p>
+          <p>Is it okay to use this photo?</p>
+        </sui-modal-description>
+      </sui-modal-content>
+      <sui-modal-actions>
+        <sui-button positive @click.native="toggle">
+          OK
+        </sui-button>
+      </sui-modal-actions>
+    </sui-modal>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  export default {
+    name: 'Booking',
+    data() {
+      return { open: false };
+    },
+    props: {
+      therapy: Object
+    },
+    methods: {
+      toggle() {
+        this.open = !this.open;
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import '../../style/main.scss';
+
+  .booking-button {
+    padding: 9px 15px;
+    font-size: 12px;
+    letter-spacing: .10rem;
+    text-transform: uppercase;
+    border-right: 1px solid #e5e5e5;
+    border-bottom: 1px solid #e5e5e5;
+    border-radius: 0px;
+    background-color: white;
+    font-family: $font-family-p;
+    font-weight: $font-weight-p;
+    &:hover{
+      background-color: $light-grey;
+    }
+  }
+</style>
