6401645934f4799014c1509d41e38cd63f9b3741
rubenberna <rubenmbernardes@gmail.com>
2018-10-01 10:15:35 +0200

    feat(): about has final layout

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,6 +4,7 @@
     <div class="about">
       <div class="about container">
         <h4 class="title">Welcome</h4>
+        <p>by Greta Lowel</p>
         <div class="about-frame">
           <div class="about-bio">
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -12,9 +13,28 @@
             <img src="https://firebasestorage.googleapis.com/v0/b/greta-f481d.appspot.com/o/greta.jpg?alt=media&token=3cc6cdb2-e80c-4665-8ae2-ad762a5195fb">
           </div>
         </div>
+        <div class="about-contacts">
+          <div class="about-contacts_details">
+            <p>0485 649 035</p>
+            <p>greta.lowel@hotmail.com</p>
+            <br>
+            <p>13 Kwaenijkstraat, 3191 Boortmeerbeek</p>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <p>Connect</p>
+            <br>
+            <p>Facebook</p>
+          </div>
+          <div class="about-contacts_image">
+            <img src="https://firebasestorage.googleapis.com/v0/b/greta-f481d.appspot.com/o/corinne-kutz-157292-unsplash.jpg?alt=media&token=7a99d373-8f4d-49d9-af28-db4b9bf4fcb7">
+          </div>
+        </div>
       </div>
 
-      </div>
+    </div>
   </div>
 </template>
 
@@ -34,13 +54,12 @@
   @import '../../style/main.scss';
 
   .about {
-    background-color: $green;
+    background: #e3dfdb;
     margin-bottom: 30px;
     padding: 1px;
     margin-top: 95px;
     color: black;
     .about.container {
-
       .title {
         display: inline-block;
         border-bottom: 3px solid #231f20;
@@ -52,16 +71,16 @@
       }
       .about-frame {
         display: flex;
-        // justify-content: space-evenly;
         justify-content: center;
         height: 500px;
         margin-top: 35px;
+        margin-bottom: -44px;
 
         .about-bio {
           display: flex;
           width: 50%;
           height: 80%;
-          margin-right: 50px;
+          margin-right: 20px;
           justify-content: center;
           align-items: center;
           background: white;
@@ -73,13 +92,51 @@
           position: relative;
           overflow: hidden;
           margin-right: 1px;
-
           img {
             position: absolute;
             border-radius: 4px;
             margin-left: -37px;
             left: 60%;
             top: 47%;
+            width: 100%;
+            height: auto;
+            -webkit-transform: translate(-50%,-50%);
+            -ms-transform: translate(-50%,-50%);
+            transform: translate(-50%,-50%);
+          }
+        }
+      }
+      .about-contacts {
+        display: flex;
+        .about-contacts_details {
+          padding: 40px;
+          vertical-align: middle;
+          background: white;
+          text-transform: uppercase;
+          height: 386px;
+          width: 360px;
+          p {
+            display: inline-block;
+            border-bottom: 3px solid #231f20;
+            font-weight: 700;
+            line-height: 1em;
+            letter-spacing: .135em;
+            padding-bottom: .125em;
+            text-transform: uppercase;
+            font-size: 16px;
+
+          }
+        }
+        .about-contacts_image {
+          width: 100%;
+          height: inherit;
+          position: relative;
+          overflow: hidden;
+          margin-right: 1px;
+          img {
+            position: absolute;
+            left: 50%;
+            top: 67%;
             width: 100%;
             height: auto;
             -webkit-transform: translate(-50%,-50%);
