a1aeda8783d56430fd1da028d17cc78c4c346862
rubenberna <rubenmbernardes@gmail.com>
2018-10-02 15:41:41 +0200

    feat(): improved layout of show page

--- src/views/ShowTherapy.vue
+++ src/views/ShowTherapy.vue
@@ -1,8 +1,39 @@
 <template>
   <div>
     <NavbarGrey/>
-    <div class="show" :style="{ backgroundImage: `url('${therapy.image}')` }">
-      <h3>{{ therapy.name }}</h3>
+    <div class="show" :style="{ backgroundImage: `linear-gradient(-225deg, rgba(0, 90, 167, 0.4) 0%, rgba(255, 253, 228, 0.4) 50%), url('${therapy.image}')` }">
+      <h3 class="show-title">{{ therapy.name }}</h3>
+
+      <div class="show-board">
+        <div class="show-board-item">
+          <h5 class="show-board-item_title">What is it?</h5>
+          <p class="show-board-item_text">{{ therapy.description }}</p>
+        </div>
+        <div class="show-board-item">
+          <h5 class="show-board-item_title">How does it work?</h5>
+          <p class="show-board-item_text">{{ therapy.method }}</p>
+        </div>
+        <div class="show-board-item">
+          <h5 class="show-board-item_title">For who is it?</h5>
+          <p class="show-board-item_text">{{ therapy.indications }}</p>
+        </div>
+      </div>
+
+      <div class="show-knobs">
+        <div class="show-knobs_price">
+
+        </div>
+        <div class="show-knobs_availability">
+
+        </div>
+        <div class="show-knobs_book">
+
+        </div>
+      </div>
+
+      <div class="show-map">
+
+      </div>
 
     </div>
   </div>
@@ -33,6 +64,45 @@
     background-repeat: no-repeat;
     background-attachment: fixed;
     background-position: center;
+    display: flex;
+    flex-direction: column;
+    .show-title {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 35%;
+      color: white;
+      font-weight: 600;
+      letter-spacing: .135em;
+    }
+    .show-board {
+      display: flex;
+      justify-content: center;
+      height: 700px;
+      .show-board-item {
+        display: flex;
+        flex-direction: column;
+        margin: 20px;
+        width: 18%;
+        height: 100%;
+        align-items: center;
+        background: white;
+        padding: 60px;
+        .show-board-item_title {
+          display: inline-block;
+          border-bottom: 3px solid #231f20;
+          font-weight: 700;
+          line-height: 1em;
+          letter-spacing: .135em;
+          padding-bottom: .125em;
+          text-transform: uppercase;
+        }
+        .show-board-item_text {
+          color: grey;
+          margin-top: 10px;
+        }
+      }
+    }
   }
 
 
