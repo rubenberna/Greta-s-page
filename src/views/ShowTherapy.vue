38da074ed5cd61ed0141daca2b70dd150b51b9c6
rubenberna <rubenmbernardes@gmail.com>
2018-10-03 12:21:37 +0200

    feat(): added knobs in show page and changed gradient

--- src/views/ShowTherapy.vue
+++ src/views/ShowTherapy.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <NavbarGrey/>
-    <div class="show" :style="{ backgroundImage: `linear-gradient(-225deg, rgba(0, 90, 167, 0.4) 0%, rgba(255, 253, 228, 0.4) 50%), url('${therapy.image}')` }">
+    <div class="show" :style="{ backgroundImage: `linear-gradient(-225deg, rgba(96, 56, 19, 0.5) 0%, rgba(178, 159, 148, 0.5) 50%), url('${therapy.image}')` }">
       <h3 class="show-title">{{ therapy.name }}</h3>
 
       <div class="show-board">
@@ -14,20 +14,24 @@
           <p class="show-board-item_text">{{ therapy.method }}</p>
         </div>
         <div class="show-board-item">
-          <h5 class="show-board-item_title">For who is it?</h5>
+          <h5 class="show-board-item_title">Who is it for?</h5>
           <p class="show-board-item_text">{{ therapy.indications }}</p>
         </div>
       </div>
 
-      <div class="show-knobs">
-        <div class="show-knobs_price">
-
+      <div class="show-knobs container">
+        <div class="show-knobs_item">
+          <img src="../../style/assets/price.png"
+               class="image-price">
+          <span>{{ therapy.price }}€</span>
         </div>
-        <div class="show-knobs_availability">
-
+        <div class="show-knobs_item">
+          <img src="../../style/assets/calendar.png">
+          <span>Availability: {{ therapy.availability }}</span>
         </div>
-        <div class="show-knobs_book">
-
+        <div class="show-knobs_item">
+          <i class="far fa-check-square"></i>
+          <span>Schedule and appointment!</span>
         </div>
       </div>
 
@@ -100,6 +104,40 @@
         .show-board-item_text {
           color: grey;
           margin-top: 10px;
+        }
+      }
+    }
+    .show-knobs {
+      display: flex;
+      justify-content: space-evenly;
+      align-items: center;
+      margin-top: 50px;
+      .show-knobs_item {
+        color: white;
+        font-size: 17px;
+        font-weight: bolder;
+        margin-left: 60px;
+        &:first-of-type {
+          margin-left: 90px;
+          img {
+            filter: brightness(0) invert(1);
+          }
+        }
+        &:nth-of-type(2) {
+          margin-left: 95px;
+          img {
+            filter: brightness(0) invert(1);
+            width: 23%;
+          }
+        }
+        &:nth-of-type(3){
+          display: flex;
+          align-items: center;
+          margin-left: -15px;
+          i {
+            font-size: 40px;
+            margin-right: 10px;
+          }
         }
       }
     }
