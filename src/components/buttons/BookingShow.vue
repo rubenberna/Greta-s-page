65f2f9f68ce0a53c188c93c55743fd1027eb487e
Ruben <rubenmbernardes@gmail.com>
2018-11-28 13:54:41 +0100

    chore: updated booking-show button with same details as booking-home

--- src/components/buttons/BookingShow.vue
+++ src/components/buttons/BookingShow.vue
@@ -1,17 +1,10 @@
 <template lang="html">
   <div class="booking">
-    <div class="currentUser"
-         v-if="currentUser">
-         <sui-button  class="booking-show-button"
-                      @click.native="toggle">
-                      Schedule appointment
+    <div class="currentUser">
+       <sui-button  class="booking-home-button"
+                    @click.native="toggle">
+                    Schedule appointment
        </sui-button>
-    </div>
-    <div v-else>
-      <sui-button  class="booking-show-button"
-                   @click="loginFirst">
-                   Schedule appointment
-    </sui-button>
     </div>
     <sui-modal v-model="open">
       <sui-modal-content image>
@@ -28,39 +21,49 @@
             </sui-header>
             <div class="booking-description">
               <p>Thank you for choosing the {{ therapy.name }} therapy!</p>
-              <p>This treatment is available on {{ therapy.availability.toLowerCase() }} with a price of €{{ therapy.price }} per session.</p>
-              <p>Would you like to schedule an appointment?</p>
+              <p>This treatment is available on {{ therapy.availability.toLowerCase() }} at €{{ therapy.price }} per session.</p>
+              <p>Please enter your details to make an appointment.</p>
             </div>
-            <sui-form class="booking-details">
-              <sui-form-field>
-                <label>Preferred date</label>
-                <input v-model="booking.date" type="date" placeholder="dd.mm.jj">
-              </sui-form-field>
-              <sui-form-field>
-                <label>Phone number</label>
-                <input v-model="booking.phone" type="tel" placeholder="0489 555 555">
-              </sui-form-field>
-              <sui-label v-if='outcome.error'
-                         basic color="red" pointing='above'
-                         class="notification">
-                         {{outcome.errorMsg}}
-              </sui-label>
-              <sui-button v-if='!outcome.success'
-                          primary
-                          @click.prevent='validateBooking()'>
-                          Schedule appointment
-              </sui-button>
-              <div v-else
-                   class="booking-success">
-                   <paragraph size='l'
-                              weight='semibold'>
-                              Thank you!!
-                  </paragraph>
-                   <paragraph size='l'
-                              weight='semibold'>
-                              I will get in touch with you shortly!
-                  </paragraph>
+            <sui-form>
+              <div class="booking-details">
+                <sui-form-field v-if='!currentUser'>
+                  <label>Email address</label>
+                  <input v-model="booking.email" type="email" placeholder="your@email.com" class="booking-form-input">
+                </sui-form-field>
+                <sui-form-field>
+                  <label>Phone number</label>
+                  <input v-model="booking.phone" type="tel" placeholder="0489 555 555" class="booking-form-input">
+                </sui-form-field>
               </div>
+
+              <div class="booking-details">
+                <sui-form-field>
+                  <label>Preferred date</label>
+                  <input v-model="booking.date" type="date" placeholder="dd.mm.jj" class="booking-form-input">
+                </sui-form-field>
+                <sui-button type='submit'
+                              v-if='!outcome.success'
+                              class='schedule-button'
+                              @click.prevent='validateBooking()'>
+                              Schedule appointment
+                 </sui-button>
+                 <div v-else
+                       class="booking-success">
+                    <paragraph size='l'
+                               weight='semibold'>
+                               Thank you!!
+                    </paragraph>
+                    <paragraph size='l'
+                               weight='semibold'>
+                               I will get in touch with you shortly!
+                    </paragraph>
+                  </div>
+                </div>
+                <sui-label v-if='outcome.error'
+                           basic color="red" pointing='above'
+                           class="notification">
+                           {{outcome.errorMsg}}
+                </sui-label>
             </sui-form>
           </sui-modal-description>
       </sui-modal-content>
@@ -88,6 +91,7 @@
           phone: null,
           therapyId: null,
           therapyName: null,
+          email: null
         },
         outcome: {
           success: false,
@@ -108,14 +112,11 @@
       toggle() {
         this.open = !this.open;
       },
-      loginFirst() {
-        router.push({ name: 'login', params: { loginMsg: 'Please login or create an account first ;)'}})
-      },
       validateBooking() {
         if (!this.booking.phone || !this.booking.date) {
           this.outcome.error = true
           this.outcome.errorMsg = null
-          this.outcome.errorMsg = 'Please fill in both fields first'
+          this.outcome.errorMsg = 'Please fill all fields'
         // } else if (!validatePhone(this.booking.phone) || !validateMobile(this.booking.phone)) {
         //   this.outcome.errorMsg = null
         //   this.outcome.errorMsg = 'Please enter a valid Belgian number'
@@ -127,8 +128,14 @@
         }
       },
       makeBooking() {
-        this.booking.userId = this.currentUser.uid
-        this.booking.clientName = this.profile.name
+        if (this.currentUser) {
+          this.booking.userId = this.currentUser.uid
+          this.booking.clientName = this.profile.name
+          this.booking.email = this.profile.email
+        } else {
+          this.booking.userId = "no account"
+          this.booking.clientName = "no account"
+        }
         this.booking.therapyId = this.therapy.id
         this.booking.therapyName = this.therapy.name
         this.createBooking(this.booking);
